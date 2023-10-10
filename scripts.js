3b9d4b3ea939329edc1a356ce95fa3f6dd7b0c5e
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-10-10 05:47:59 +0200

    Edited the code to meet unresolved scenario 2

--- scripts.js
+++ scripts.js
@@ -7,10 +7,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
 
-  // Calculate the division result and round it down to the nearest whole number
-  const divisionResult = Math.floor(dividend / divider);
-
-  result.innerText = divisionResult;
+  // Check if either input is empty
+  if (dividend.trim() === "" || divider.trim() === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+  } else {
+    // Calculate the division result and round it down to the nearest whole number
+    const divisionResult = Math.floor(dividend / divider);
+    result.innerText = divisionResult;
+  }
 });
