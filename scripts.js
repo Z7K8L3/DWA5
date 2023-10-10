c80ee6ae8360d50981723e899be84ae73a157c94
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-10-10 05:49:28 +0200

    Edited the code to meet unresolved scenario 3

--- scripts.js
+++ scripts.js
@@ -8,12 +8,19 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  // Check if either input is empty
-  if (dividend.trim() === "" || divider.trim() === "") {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+  // Convert inputs to numbers and check for validity
+  const dividendNumber = parseFloat(dividend);
+  const dividerNumber = parseFloat(divider);
+
+  // Check if either input is not a valid number or if the divider is 0
+  if (isNaN(dividendNumber) || isNaN(dividerNumber) || dividerNumber === 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    
+    // Log the error to the console with the call stack
+    console.error("Error: Invalid division attempted", new Error().stack);
   } else {
     // Calculate the division result and round it down to the nearest whole number
-    const divisionResult = Math.floor(dividend / divider);
+    const divisionResult = Math.floor(dividendNumber / dividerNumber);
     result.innerText = divisionResult;
   }
 });
